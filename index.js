6dab4ccaedc70461a2c5e669c403c5a916264fe3
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-30 21:51:18 +0200

    Installed express and created first routes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Index file");
+const express = require("express");
+
+const PORT = 3000;
+const server = express();
+
+server.use("/pokemons", (req, res) => {
+  const pokemons = ["Picachu", "Raichu"];
+  const pokemonsString = JSON.stringify(pokemons);
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.send(pokemonsString);
+});
+
+server.use("/", (req, res) => {
+  res.send("Hola mundo!! Esto es un proyecto con express!!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor está levantado y escuchando en el puerto ${PORT}`);
+});
