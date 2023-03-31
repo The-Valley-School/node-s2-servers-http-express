4e62c70afe02db06cdb395e5bc683a3a64955f30
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-31 17:39:39 +0200

    Added router

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const express = require("express");
 
 const PORT = 3000;
 const server = express();
+const router = express.Router();
 
-server.use("/pokemons", (req, res) => {
-  const pokemons = ["Picachu", "Raichu"];
-  const pokemonsString = JSON.stringify(pokemons);
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  res.send(pokemonsString);
+const pokemons = ["Picachu", "Raichu", "Charmander", "Charmeleon", "Bulbasaur"];
+
+router.get("/", (req, res) => {
+  res.send("Este es un ejemplo de router de Express");
 });
 
-server.use("/", (req, res) => {
-  res.send("Hola mundo!! Esto es un proyecto con express!!");
+router.get("/pokemons", (req, res) => {
+  res.json(pokemons);
 });
+
+router.get("/pokemons/:position", (req, res) => {
+  const position = req.params.position;
+
+  if (typeof position !== "undefined" && pokemons[position]) {
+    res.send(pokemons[position]);
+  } else {
+    res.status(404);
+    res.send("Pokemon con encontrado.");
+  }
+});
+
+server.use("/", router);
 
 server.listen(PORT, () => {
   console.log(`Servidor está levantado y escuchando en el puerto ${PORT}`);
